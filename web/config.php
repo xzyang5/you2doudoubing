bcea4c9809abed65fc4c0b508b8e807041c8d511
xzyang5 <36848432+xzyang5@users.noreply.github.com>
2019-01-17 23:23:08 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
+define('APIKEYAIzaSyCNXTNAsWLorWzPx6LP-nsTDTuABThwNSM');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
+define('SITE_NAME', 'love2you');
 define('TITLENAME', 'DouDouTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'xz123621@gmail.com');
 ?>
